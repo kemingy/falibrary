aee45d09c996563f1997f5a5fc430d722b3bfd9b
keming <kemingy94@gmail.com>
2019-09-18 17:46:10 +0800

    init library

---
+++ falibrary/config.py
@@ -0,0 +1,13 @@
+class Config:
+    def __init__(self):
+        self.NETLOC = 'apidoc'  #: netloc for API document page
+        self.TEMPLATE_FOLDER = 'templates'
+        self.FILENAME = 'openapi.json'
+        self.OPENAPI_VERSION = '3.0.2'  #: OpenAPI version
+
+        self.TITLE = 'Service API Document'  #: service name
+        self.VERSION = '0.1'  #: service version
+        self.DOMAIN = None  #: service host domain
+
+
+default_config = Config()
