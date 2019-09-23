a27ed3240b394f04420ceaaa516223515ae60e27
keming <kemingy94@gmail.com>
2019-09-23 20:06:16 +0800

    add example

---
+++ examples/demo.py
@@ -0,0 +1,51 @@
+import falcon
+from wsgiref import simple_server
+from pydantic import BaseModel, Schema
+from random import random
+
+from falibrary import Falibrary
+
+
+app = falcon.API()
+api = Falibrary(
+    app,
+    title='Demo Service',
+    version='0.1.2',
+)
+
+
+class Query(BaseModel):
+    text: str
+
+
+class Response(BaseModel):
+    label: int
+    score: float = Schema(
+        ...,
+        gt=0,
+        lt=1,
+    )
+
+
+class Data(BaseModel):
+    uid: str
+    limit: int
+    vip: bool
+
+
+class Classification():
+    @api.validate(query=Query, data=Data, response=Response, x=[falcon.HTTP_403])
+    def on_post(self, req, resp, source, target):
+        print(f'{source} => {target}')
+        print(req.context.query)
+        print(req.context.data)
+        if random() < 0.5:
+            raise falcon.HTTPForbidden("Bad luck. You're fobidden.")
+        return Response(label=int(10 * random()), score=random())
+
+
+app.add_route('/api/{source}/{target}', Classification())
+
+if __name__ == '__main__':
+    httpd = simple_server.make_server('localhost', 8000, app)
+    httpd.serve_forever()
