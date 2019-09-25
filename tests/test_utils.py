c2df22df253022096ccc7f3152aa6c505bfe2718
keming <kemingy94@gmail.com>
2019-09-25 16:31:12 +0800

    add test

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from falibrary.utils import parse_path
+
+
+def test_parse():
+    # non variable
+    uri = '/api/predict'
+    path, param = parse_path(uri)
+    assert path == uri
+    assert param == []
