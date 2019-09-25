0910f79254b226e605f530e489f3ebb7af0ba0c6
keming <kemingy94@gmail.com>
2019-09-25 16:17:21 +0800

    fix HTML, add class name

--- falibrary/route.py
+++ falibrary/route.py
@@ -2,6 +2,7 @@
 route for API document page and OpenAPI spec
 """
 import os
+import re
 
 
 class RedocPage:
@@ -9,12 +10,14 @@ class RedocPage:
         self.config = config
 
     def on_get(self, req, resp):
-        resp.content_type = 'text/html; charset=utf-8'
+        resp.content_type = 'text/html'
         with open(os.path.join(os.getcwd(),
                                'falibrary',
                                self.config.TEMPLATE_FOLDER,
                                'redoc.html'), 'r', encoding='utf-8') as f:
-            resp.body = f.read()
+            page = f.read()
+
+        resp.body = re.sub('{{}}', self.config.SPEC_URL, page)
 
 
 class OpenAPI:
@@ -23,3 +26,6 @@ class OpenAPI:
 
     def on_get(self, req, resp):
         resp.media = self.api.spec
+
+
+_doc_class_name = [x.__name__ for x in (RedocPage, OpenAPI)]
