aa596486040f1ac08d4c748c1d7d94afd0e3e81d
CaptainLazarus <adityagudimetla@gmail.com>
2019-10-03 19:44:49 +0530

    Added unit test for testing config

---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+from falibrary.config import default_config
+from falibrary import Falibrary
+
+def test_config():
+
+    #Temporary Unit test. Will spruce this up later.
+    test = Falibrary(
+        title='Machine Translation',
+        version='1.2.0',
+        UI = 'swagger',
+        OPENAPI_VERSION = '2.0.1'
+    )
+
+    assert test.config.UI == 'swagger' , 'UI mismatch'
+    assert test.config.TITLE == 'Machine Translation' , 'Title mismatch'
+    assert test.config.VERSION == '1.2.0' , 'Version mismatch'
+    assert test.config.FILENAME == default_config.FILENAME , 'Filename mismatch'
+    assert test.config.OPENAPI_VERSION == '2.0.1' , 'API version mismatch'
+
+if __name__ == "__main__":
+    test_config()
+    print("Config pass")
