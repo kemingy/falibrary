b3bc54222ddacc1f133ad01a7a2893133fdc6b3a
keming <kemingy94@gmail.com>
2019-10-05 20:59:10 +0800

    fix bug: 'query' to 'data' in generate_spec add simple demo

---
+++ examples/simple.py
@@ -0,0 +1,31 @@
+import falcon
+from wsgiref import simple_server
+from pydantic import BaseModel, Schema
+from random import random
+
+from falibrary import Falibrary
+
+api = Falibrary(
+    title='Demo Service',
+    version='0.1.2',
+)
+
+
+class Query(BaseModel):
+    text: str
+
+
+class Demo():
+    @api.validate(query=Query)
+    def on_post(self, req, resp):
+        print(req.context.query)
+        pass
+
+
+if __name__ == '__main__':
+    app = falcon.API()
+    app.add_route('/api/demo', Demo())
+    api.register(app)
+
+    httpd = simple_server.make_server('localhost', 8000, app)
+    httpd.serve_forever()
